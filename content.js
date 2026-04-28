de2ebf2df1f2699fa196a6209c84f94d449aa860
gloomcowboy <phillshaw8004321@gmail.com>
2026-04-28 15:17:34 +0100

    Fixed, playlists weren't showing up

--- content.js
+++ content.js
@@ -4,9 +4,31 @@ if (window.location.href === 'https://www.youtube.com/' ||
   window.location.replace('https://www.youtube.com/feed/subscriptions');
 }
 
-const observer = new MutationObserver(() => { //  MutationObserver -> browser API that watches the page for any DOM changes (new elements appearing, things moving around)
+function isWatchingPLaylist() {
+  const params = new URLSearchParams(window.location.search);
+  return params.has('list'); 
+}
+
+function updateSideBar() {
   const sidebar = document.querySelector('#secondary');
-  if (sidebar) sidebar.style.display = 'none';
-});
-// Observe -> starts watching the entire page for changes
-observer.observe(document.documentElement, { childList: true, subtree: true });
+  if (!sidebar) return;
+
+  if (isWatchingPLaylist()) {
+    //show side bar if its a playlist
+    sidebar.style.display = '';
+
+    const recommended = sidebar.querySelector('#related');
+    if (recommended) recommended.style.display = 'none';
+
+    const autoplay = sidebar.querySelector('ytd-compact-autoplay-renderer');
+    if (autoplay) autoplay.recommended.style.display = 'none';
+  } else {
+    //no playlist hide the sidebar
+    sidebar.style.display = 'none';
+  }
+}
+
+const observer = new MutationObserver(updateSideBar);
+observer.observe(document.documentElement, {childList: true, subtree: true });
+
+window.addEventListener('yt-navigate-finish', updateSideBar);
